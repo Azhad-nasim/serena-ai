0f9bb862d4530347b6900a369827be1e9451903b
AZHAD <azhadnasim.786@gmail.com>
2025-06-30 18:53:53 +0530

    env changes

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,23 +1,19 @@
-// Import the functions you need from the SDKs you need
-
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ofDFt3veYQE1HB5DtNu8iT1vmp_21uY",
-  authDomain: "serenaai-3bb19.firebaseapp.com",
-  databaseURL:
-    "https://serenaai-3bb19-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "serenaai-3bb19",
-  storageBucket: "serenaai-3bb19.firebasestorage.app",
-  messagingSenderId: "854143464515",
-  appId: "1:854143464515:web:1238332cae6d319d56d0f6",
-  measurementId: "G-V04V1HVBEC",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
